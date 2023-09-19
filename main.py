109f176a65464064f719e00692d72f0ac37ace58
Coolstormaction <halderhena05@gmail.com>
2023-09-19 14:18:56 +0530

    initial commit

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from rich.console import Console
 import pyfiglet, platform, os, random, sys
 
 os.system('cls' if os.name == 'nt' else 'clear')
+os_ = platform.system()
+osname = os.name
 console = Console()
+
+
+console.print(f'{os_}@{osname.capitalize()}')
