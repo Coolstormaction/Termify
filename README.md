78dd6eeb9bdd542ac3666dccab606f630cbc2f15
Coolstormaction <halderhena05@gmail.com>
2023-09-19 13:56:51 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Termify
+
+The supersuccesor to my own 'unix-commands-clone'
+Currently in development.
---
+++ main.py
@@ -0,0 +1,5 @@
+from rich.console import Console 
+import pyfiglet, platform, os, random, sys
+
+os.system('cls' if os.name == 'nt' else 'clear')
+console = Console()
