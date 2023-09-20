34dfcddf1d1d509cd2c4f1510c208866a6465680
Coolstormaction <halderhena05@gmail.com>
2023-09-20 16:24:19 +0530

    command bug -> to be fixed

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 run: 
 	py main.py
+
+build: 
+	@read -p 'Enter your commit message: ' commit_message; \
+	git add .; \
+	git commit -m "$$commit_message"; \
+	git push origin master
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from rich.console import Console 
+from pathlib import Path
 import pyfiglet, platform, os, random, sys
 
 os.system('cls' if os.name == 'nt' else 'clear')
@@ -6,11 +7,35 @@ os_ = platform.system()
 osname = os.name
 console = Console()
 
-
-console.print(f'{os_}@{osname.capitalize()}', end='')
+print(pyfiglet.figlet_format('TERMIFY'))
+console.print(f'{os_}@{osname.capitalize()}')
 inp = input('')
 
-if inp == 'ls': 
-    for i in os.listdir(): 
-        if i == '.git': continue
-        if os.path.isfile(i): print('path is file')
+while True:
+    if inp == 'ls': 
+        for i in os.listdir(): 
+            try: 
+                if i == '.git': continue
+            except Exception: continue
+            if os.path.isfile(i): console.print('[dark_cyan]--file-- ', end='')
+            if os.path.isdir(i): console.print('[purple4]--dir-- ', end='')
+            console.print(i)
+
+    if inp.startswith('cd '):
+        nextDir = inp.split()
+        if inp.split('cd')[1] == '..': ...
+
+        os.chdir(nextDir)
+
+    if inp.startswith('dl '): 
+        deleteDir = inp.split()[1]
+        Path.rmdir(deleteDir)
+
+        console.print('success!')
+
+    if inp.startswith('mkd '): 
+        dirToBeCreated = inp.split()[1]
+        os.mkdir(dirToBeCreated)
+
+    
+        
